aef245a3c8a396bf2f087c6c5c6ae31e02f9c232
Srikanth Subrmanian <srikanth.subramanian@imaginea.com>
2014-03-13 10:54:39 +0530

    eShopping: Added Main.html and Main.js

--- webapproot/pages/Main/Main.js
+++ webapproot/pages/Main/Main.js
@@ -1,4 +1,34 @@
-Application.$controller("MainPageController", ['$scope', 'Widgets', 'Variables', function ($scope, Widgets, Variables) {
-    "use strict";
+Application.$controller("MainPageController", ['$scope', 'Widgets', 'Variables', '$timeout',
+    function($scope, Widgets, Variables, $timeout) {
+        "use strict";
 
-}]);
+        var views = ['view1', 'view2', 'view3'],
+            viewName,
+            index,
+            bannerTimeout = 1000;
+
+        var toggleViews = function(index) {
+            if (index > views.length - 1) {
+                index = 0;
+            }
+            viewName = views[index];
+            $('.dealsBanner .app-view').hide();
+            $('#' + viewName).show();
+            $timeout(function() {
+                toggleViews(index + 1);
+            }, bannerTimeout);
+
+        }
+
+        var displayViews = function() {
+            index = 0;
+            $('.dealsBanner .app-view').hide();
+            $("#" + views[index]).show();
+            $timeout(function() {
+                toggleViews(index + 1);
+            });
+
+        }();
+
+    }
+]);

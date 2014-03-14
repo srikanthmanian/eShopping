f6e6889cf9396efa95c25a0e6649402802c276bb
jananij <janani.j@imaginea.com>
2014-03-14 15:40:16 +0530

    dashboard page added

---
+++ webapproot/pages/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+Application.$controller("DashboardPageController", ["$scope", "Widgets", function ($scope, Widgets) {
+	"use strict";
+
+}]);
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+	}
+]);

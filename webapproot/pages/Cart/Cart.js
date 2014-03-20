0aa7fcfd6e5f756ab7a24e3264a60dbf356920a3
jananij <janani.j@imaginea.com>
2014-03-20 23:00:14 +0530

    variables accessed on Variableready event, related cache issues fixed

--- webapproot/pages/Cart/Cart.js
+++ webapproot/pages/Cart/Cart.js
@@ -4,9 +4,10 @@ Application.$controller("CartPageController", ["$scope", "$rootScope", "Widgets"
         (function() {
             $rootScope.userLoggedin = Utils.browserStorage.getItem('wm.isUserLoggedIn');
         })();
-
         $scope.onPageReady = function() {
             $rootScope.pageLoading = false;
+        }
+        $scope.onPageVariablesReady = function() {
             Variables.currentUser.dataSet = JSON.parse(Utils.browserStorage.getItem('wm.currentUserObj'));
         }
     }
